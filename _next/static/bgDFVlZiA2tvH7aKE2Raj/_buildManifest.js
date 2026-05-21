56fe225328794b46f85f544e77390c5938774a40
Emir Kibar <62299912+kibradev@users.noreply.github.com>
2026-05-21 16:14:52 +0300

    as

---
+++ _next/static/bgDFVlZiA2tvH7aKE2Raj/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/smartpad-landingsite//_next/:path+",
+        "destination": "/smartpad-landingsite/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
